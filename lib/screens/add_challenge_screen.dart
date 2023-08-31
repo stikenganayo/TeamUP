2fa86846541ba9c38e3b62a762a05b1545af1f82
cjf646 <cjf646@uregina.ca>
2023-08-31 00:27:11 -0600

    updated challenges and events page

--- lib/screens/add_challenge_screen.dart
+++ lib/screens/add_challenge_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class CreateChallenge extends StatelessWidget {
+class CreateChallenge extends StatefulWidget {
   const CreateChallenge({Key? key}) : super(key: key);
 
   @override
+  _CreateChallengeState createState() => _CreateChallengeState();
+}
 
+class _CreateChallengeState extends State<CreateChallenge> {
+  List<ChallengeData> challengeDataList = [
+    ChallengeData(challengeTitle: "", controller: TextEditingController())
+  ];
+
+  String selectedFrequency = "every day"; // Default value
+  List<String> frequencyOptions = [
+    "every day",
+    "every 2 days",
+    "every 3 days",
+    "every 4 days",
+    "every 5 days",
+    "every 6 days",
+    "every week",
+    "every 2 weeks",
+    // Add more options as needed
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,37 +31,115 @@ class CreateChallenge extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add Challenge!'),
       ),
-      body: const Center(
-        child: Text(
-          'OK Coach, create the challenge',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Column(
+                children: challengeDataList
+                    .map((data) => _buildChallengeRow(data))
+                    .toList(),
+              ),
+              const SizedBox(height: 20),
+              _buildFrequencySection(),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: areFieldsFilled() ? () => postChallenge() : null,
+                child: Text('Post Challenge'),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
 
+  Widget _buildChallengeRow(ChallengeData data) {
+    return Row(
+      children: [
+        const Icon(Icons.check_box_outline_blank),
+        const SizedBox(width: 10),
+        Flexible(
+          child: TextField(
+            onChanged: (value) {
+              data.challengeTitle = value;
+            },
+            decoration: const InputDecoration(
+              hintText: 'Challenge',
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            controller: data.controller,
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              challengeDataList.add(
+                ChallengeData(challengeTitle: "", controller: TextEditingController()),
+              );
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFrequencySection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Set Frequency',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          child: DropdownButton<String>(
+            value: selectedFrequency,
+            onChanged: (String? newValue) {
+              setState(() {
+                selectedFrequency = newValue!;
+              });
+            },
+            items: frequencyOptions.map<DropdownMenuItem<String>>(
+                  (String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              },
+            ).toList(),
+            hint: Text('Select Frequency'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  bool areFieldsFilled() {
+    return challengeDataList.isNotEmpty &&
+        challengeDataList.every((data) => data.challengeTitle.isNotEmpty) &&
+        selectedFrequency.isNotEmpty;
+  }
+
+  void postChallenge() {
+    // Implement your logic to post the challenge here
+    print('Challenges Posted: ${challengeDataList.map((data) => data.challengeTitle).toList()}');
+    print('Selected Frequency: $selectedFrequency');
   }
 }
 
+class ChallengeData {
+  String challengeTitle;
+  TextEditingController controller;
 
-// class CreateChallenge extends StatelessWidget {
-//   const CreateChallenge({Key? key}) : super(key: key);
-//
-//   @override
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Create Challenge!'),
-//       ),
-//       body: const Center(
-//         child: Text(
-//           'OK Coach, create the Challenge',
-//           style: TextStyle(fontSize: 20),
-//         ),
-//       ),
-//     );
-//
-//   }
-// }
+  ChallengeData({required this.challengeTitle, required this.controller});
+}
--- lib/screens/add_event_screen.dart
+++ lib/screens/add_event_screen.dart
@@ -1,11 +1,67 @@
 import 'package:flutter/material.dart';
 
-
-class CreateEvent extends StatelessWidget {
+class CreateEvent extends StatefulWidget {
   const CreateEvent({Key? key}) : super(key: key);
 
   @override
+  _CreateEventState createState() => _CreateEventState();
+}
 
+class _CreateEventState extends State<CreateEvent> {
+  String eventTitle = "";
+  DateTime startDate = DateTime.now();
+  TimeOfDay startTime = TimeOfDay.now();
+  DateTime endDate = DateTime.now();
+  TimeOfDay endTime = TimeOfDay.now();
+  String eventLocation = "";
+
+  Future<void> _selectStartDate(BuildContext context) async {
+    final DateTime pickedDate = (await showDatePicker(
+      context: context,
+      initialDate: startDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    ))!;
+    if (pickedDate != null && pickedDate != startDate)
+      setState(() {
+        startDate = pickedDate;
+      });
+  }
+
+  Future<void> _selectStartTime(BuildContext context) async {
+    final TimeOfDay pickedTime = (await showTimePicker(
+      context: context,
+      initialTime: startTime,
+    ))!;
+    if (pickedTime != null && pickedTime != startTime)
+      setState(() {
+        startTime = pickedTime;
+      });
+  }
+
+  Future<void> _selectEndDate(BuildContext context) async {
+    final DateTime pickedDate = (await showDatePicker(
+      context: context,
+      initialDate: endDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    ))!;
+    if (pickedDate != null && pickedDate != endDate)
+      setState(() {
+        endDate = pickedDate;
+      });
+  }
+
+  Future<void> _selectEndTime(BuildContext context) async {
+    final TimeOfDay pickedTime = (await showTimePicker(
+      context: context,
+      initialTime: endTime,
+    ))!;
+    if (pickedTime != null && pickedTime != endTime)
+      setState(() {
+        endTime = pickedTime;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +69,81 @@ class CreateEvent extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Create Event!'),
       ),
-      body: const Center(
-        child: Text(
-          'OK Coach, create the Event',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  eventTitle = value;
+                });
+              },
+              decoration: InputDecoration(labelText: 'Event Title'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: <Widget>[
+                TextButton(
+                  onPressed: () => _selectStartDate(context),
+                  child: Text(
+                    'Select Start Date',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Text(
+                  '${startDate.toLocal()}'.split(' ')[0],
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                TextButton(
+                  onPressed: () => _selectStartTime(context),
+                  child: Text(
+                    'Select Start Time',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Text(
+                  '${startTime.format(context)}',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+
+            Row(
+              children: <Widget>[
+                TextButton(
+                  onPressed: () => _selectEndTime(context),
+                  child: Text(
+                    'Select End Time',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Text(
+                  '${endTime.format(context)}',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  eventLocation = value;
+                });
+              },
+              decoration: InputDecoration(labelText: 'Event Location'),
+            ),
+          ],
         ),
       ),
     );
-
   }
 }
