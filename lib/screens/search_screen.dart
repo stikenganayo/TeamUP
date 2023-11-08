3c4f9b4f86dec8ac7f621c1b80e0ee9d5c582f68
Sharmarke Hassan <113717171+stikenganayo@users.noreply.github.com>
2023-11-07 22:26:56 -0600

    Added firebase functionality to add and remove friends

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,7 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  List<String> searchResults = [];
+
+  void _searchUsers(String query) {
+    FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isGreaterThanOrEqualTo: query)
+        .where('email', isLessThan: query + 'z')
+        .get()
+        .then((QuerySnapshot<Map<String, dynamic>> usersQuery) {
+      setState(() {
+        searchResults = usersQuery.docs.map((doc) => doc['email'] as String).toList();
+      });
+    }).catchError((error) {
+      print('Firebase Search Error: $error');
+    });
+  }
+
+  void _addUser(String user) {
+    // Store the user in Firestore
+    FirebaseFirestore.instance.collection('user_friends').add({
+      'user_email': FirebaseAuth.instance.currentUser?.email,
+      'friend_email': user,
+    }).then((value) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('$user added!')),
+      );
+    }).catchError((error) {
+      print('Firebase Add User Error: $error');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,69 +48,35 @@ class SearchScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add Friends'),
       ),
-      body: const SearchContent(),
-    );
-  }
-}
-
-class SearchContent extends StatefulWidget {
-  const SearchContent({Key? key}) : super(key: key);
-
-  @override
-  _SearchContentState createState() => _SearchContentState();
-}
-
-class _SearchContentState extends State<SearchContent> {
-  final TextEditingController _searchController = TextEditingController();
-  List<String> placeholderUsers = ['Hassan', 'Curtis', 'Tsuyog'];
-
-  List<String> searchResults = [];
-
-  void _searchUsers(String query) {
-    setState(() {
-      searchResults = placeholderUsers
-          .where((user) => user.toLowerCase().contains(query.toLowerCase()))
-          .toList();
-    });
-  }
-
-  void _addUser(String user) {
-    // Add your logic to add the user to the list or perform any other action
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('$user added!')),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            controller: _searchController,
-            onChanged: _searchUsers,
-            decoration: const InputDecoration(
-              labelText: 'Search for usernames',
-              suffixIcon: Icon(Icons.search),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: _searchUsers,
+              decoration: const InputDecoration(
+                labelText: 'Search for users by email',
+                suffixIcon: Icon(Icons.search),
+              ),
             ),
           ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: searchResults.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(searchResults[index]),
-                trailing: ElevatedButton(
-                  onPressed: () => _addUser(searchResults[index]),
-                  child: const Text('Add'),
-                ),
-              );
-            },
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchResults.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(searchResults[index]),
+                  trailing: ElevatedButton(
+                    onPressed: () => _addUser(searchResults[index]),
+                    child: const Text('Add'),
+                  ),
+                );
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
